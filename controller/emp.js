8023efc3e835efe3e1513f32b4da31cf4e91fb94
NEHA SINGH <nehasingh9411450@gmail.com>
2025-04-13 22:45:13 +0530

    updated

--- controller/emp.js
+++ controller/emp.js
@@ -1,14 +1,12 @@
 const Emp = require('../models/emp');
+const { setUser } = require("../services/service");
 
-const {setUser}=require("../services/service")
 async function empRegister(req, res) {
     const { name, email, password } = req.body;
-    if(!name || !email || !password) 
+    if (!name || !email || !password) 
         return res.status(400).json({ message: "All fields are required" });                                    
-    await Emp.create({name,email,password});
-    // return res,redirect("/login")
+    await Emp.create({ name, email, password });
     res.redirect("/login");
-
 }
 
 async function login(req, res) {
@@ -20,9 +18,10 @@ async function login(req, res) {
         return res.json({ message: "Invalid credentials" });
     }
 
-    return res.json({ message: "Login successful", user });
+    const token = setUser(user);
+    res.cookie("session_id", token);
+
+    return res.redirect('/home');
 }
-const token=setUser(user);
-res.cookie("session_id",token);
-return res.redirect('/home');
-module.exports = { empRegister,login};
+
+module.exports = { empRegister, login };
